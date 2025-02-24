c4563ec89bca2b1c13434c4a5e9220a98c171653
muqtado <mishra.coolamon@gmail.com>
2025-02-24 08:54:10 +0700

    Update script.js to fetch and show comments from google form

--- AI-Ethics-Guide/AI-Model-Guidelines/script.js
+++ AI-Ethics-Guide/AI-Model-Guidelines/script.js
@@ -1,5 +1,19 @@
 // You can add interactive features here, such as toggling visibility of detailed content
 document.addEventListener('DOMContentLoaded', function() {
+    
+    fetch('https://script.google.com/macros/s/AKfycbyjO968g4tuex86kWcpmF7ivzQU0C4uCCwLqDSd9lAcELYHj-obFXAhCPcKYMmpzna3Xg/exec')
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        const commentsList = document.getElementById('comments-list');
+        data.forEach(comment => {
+            const listItem = document.createElement('li');
+            listItem.textContent = `"${comment.Feedback}" - ${comment.Name} ${comment.Email}`;
+            commentsList.appendChild(listItem);
+        });
+    })
+    .catch(error => console.error('Error fetching comments:', error));
+    
     const sections = document.querySelectorAll('section');
     sections.forEach(section => {
         section.addEventListener('click', function() {
